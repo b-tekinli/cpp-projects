872778880e406569ab8944977a68bc74f7cbf102
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-02-18 07:02:49 +0300

    added

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>     // cout fonk. kullanabilmek icin eklenir.
+using namespace std;    // std ifadesini surekli kullanmamak icin
+
+int main() {
+    int num;
+
+    // cin --> console input'tan gelir.
+    cin >> num;         // klavyeden veri okumayi saglar.
+
+    // cout --> console output'tan gelir.
+    cout << "Girilen sayi: " << num << endl;        // ekrana yazdirmayi saglar.
+    
+    
+    /*
+    endl --> newline atar. 
+    \n ile arasında bir fark yoktur.
+    */ 
+    
+    return (0);
+}
+
