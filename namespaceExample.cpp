31f5a426f2ad11d2cbd7398811f41dd05fa4ebec
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-02-21 02:16:43 +0300

    added

---
+++ namespaceExample.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+namespace myNamespace {
+    int a = 42;
+
+    void myFunction() {
+        cout << "Hello World!" << endl;
+    }
+}
+
+using namespace myNamespace;        // burada myNamespace'i import etmezsek main fonksiyonu icinde a ve myFUnction fonk. kullanamayız.
+
+int main() {
+    cout << a << endl;
+
+    myFunction();
+
+    return (0);
+}
