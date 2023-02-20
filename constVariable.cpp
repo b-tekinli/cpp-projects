5a5a5a280baa7287dfe356bd091e71d8daeb1ebc
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-02-21 02:34:26 +0300

    added

---
+++ constVariable.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int a = 42;
+
+    cout << a << endl;
+
+    // a = 5; ==> bunu yapamayız cunku const aslında constant kelimesinin kısaltmasından gelir. sabit demektir. sabit olan bir seyi degistiremeyiz.
+
+    return (0);
+}
