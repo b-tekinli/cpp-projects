40ee6940c02afcbee9c57f6a4de74b0146f08cd3
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-02-20 14:02:29 +0300

    added

---
+++ kopekYasiniInsanYasinaCevir.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int kopekYasi;
+    cout << "Kopeginizin yasini giriniz: ";
+    cin >> kopekYasi;
+
+    int sonuc = kopekYasi * 7;
+
+    cout << "Insan yasi olarak verilen sonuc = " << sonuc << "\n";
+
+    return (0);
+}
