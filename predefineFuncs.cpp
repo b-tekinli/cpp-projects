7b7e0b29d63e8fa3ec1b1cbd7df5ef3d1e5ac1a5
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-02-21 04:26:25 +0300

    added

---
+++ predefineFuncs.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+    int a = 64;
+
+    cout << a << " sayisinin karekoku: " << sqrt(a) << endl;
+    cout << a << " sayisinin ussu: " << pow(a, 2) << endl;
+
+    // sqrt     --> karekok alir.
+    // pow      --> us alir.
+    // exit     --> programi sonlandirir ve kendisinden sonra kod yazilse bile programi sonlandiracagi icin onlari okumaz.
+
+    exit(0);
+
+    cout << "sonlandirildi !" << endl;
+}
