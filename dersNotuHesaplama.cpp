f722f6c687068fdec9ecab0c9ccc5ebc973bda81
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-02-21 03:48:53 +0300

    updated

--- dersNotuHesaplama.cpp
+++ dersNotuHesaplama.cpp
@@ -4,31 +4,72 @@ using namespace std;
 int main() {
     // 1.Vize -> %30       2. Vize -> %30       Final -> %40
 
-    float exam1, exam2, finalExam;
+    int exam1, exam2, finalExam;
 
     cout << "1. Vize notunuzu giriniz: ";
     cin >> exam1;
 
-    float examPoint1 = exam1 * 0.3;
+    int examPoint1 = exam1 * 0.3;
     cout << "1. Vizeden gelen not: " << examPoint1 << endl;
 
     
     cout << "2. Vize notunuzu giriniz: ";
     cin >> exam2;
 
-    float examPoint2 = exam2 * 0.3;
+    int examPoint2 = exam2 * 0.3;
     cout << "2. Vizeden gelen not: " << examPoint2 << endl;
 
 
     cout << "Final notunuzu giriniz: ";
     cin >> finalExam;
 
-    float finalExamPoint = finalExam * 0.4;
+    int finalExamPoint = finalExam * 0.4;
     cout << "Final notunuzdan gelen not: " << finalExamPoint << endl;
 
-    double result = examPoint1 + examPoint2 + finalExamPoint;
+    int result = examPoint1 + examPoint2 + finalExamPoint;
 
     cout << "Donem sonu notunuz: " << result << endl;
 
+
+    if (result >= 90 && result <= 100)
+    {
+        cout << "Harf notunuz AA" << endl;
+    }
+    else if (result >= 85 && result <= 89)
+    {
+        cout << "Harf notunuz BA" << endl;
+    }
+    else if (result >= 80 && result <= 84)
+    {
+        cout << "Harf notunuz BB" << endl;
+    }
+    else if (result >= 70 && result <= 79)
+    {
+        cout << "Harf notunuz CB" << endl;
+    }
+    else if (result >= 60 && result <= 69)
+    {
+        cout << "Harf notunuz CC" << endl;
+    }
+    else if (result >= 50 && result <= 59)
+    {
+        cout << "Harf notunuz DC" << endl;
+    }
+    else if (result >= 45 && result <= 49)
+    {
+        cout << "Harf notunuz DD" << endl;
+    }
+    else if (result >= 0 && result <= 44)
+    {
+        cout << "Harf notunuz FF" << endl;
+    }
+    else if (!result)
+    {
+        cout << "Harf notunuz FG -> Sinavlara girmediniz!" << endl;
+    }
+    else {
+        cout << "Yanlis ya da eksik giris yaptiniz. Lutfen tekrar deneyiniz!" << endl;
+    }
+    
     return (0);
 }
