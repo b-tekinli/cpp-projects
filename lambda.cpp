28beee0799f19a639531932df415373a1b5a898a
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-03-10 01:37:41 +0300

    updated

--- lambda.cpp
+++ lambda.cpp
@@ -3,16 +3,21 @@
 #include <algorithm>
 using namespace std;
 
-void print() {
-    cout << "Merhaba Dunya" << endl;
+int print(int a, int b) {
+    cout << "Merhaba Dunya" << endl << (a + b);
+
+    return a + b;
 }
 
 // lambda ornek : [cc](p){fb}
 
 int main() {
-    auto printL = []() { cout << "Merhaba Dunya" << endl; };
+    // auto printL = []() { cout << "Merhaba Dunya" << endl; };
+    
+    auto printL = [](int a, int b)->int { return a + b; };
+    cout << printL(5, 5);
 
-    printL();
+    // printL();
 
     return (0);
 }
