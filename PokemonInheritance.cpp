69d39766ba799bcee27d4985a8f15fbf6d17847d
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-03-09 18:42:37 +0300

    added

---
+++ PokemonInheritance.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+class Pokemon {
+    protected:
+        string isim;
+        string saldiri;
+
+    public:
+        Pokemon(string name, string attack) {
+            isim = name;
+            saldiri = attack;
+        }
+        void isimSoyle() { cout << "Benim ismim " << isim << endl; }
+        void saldir() { cout << saldiri << endl; }
+};
+
+
+class Pikacu : public Pokemon {     // pokemon sınıfını inherit aldık.
+    public:
+        Pikacu(string name, string attack) : Pokemon(name, attack) {    // base class a parametre atadık
+            
+        }
+};
+
+
+class Charmander : public Pokemon {
+    public:
+        Charmander(string name, string attack) : Pokemon(name, attack) {
+            
+        }
+};
+
+
+int main() {
+    Pikacu pikacu("Pikacu", "Yildirim saldirisi !");
+
+    cout << "Seni sectim Pikacu: ";
+    pikacu.isimSoyle();
+    pikacu.saldir();
+
+
+    Charmander charmander("Charmander", "Ates topu saldirisi !");
+
+    cout << "Seni sectim Charmander: ";
+    charmander.isimSoyle();
+    charmander.saldir();
+
+
+    return (0);
+}
