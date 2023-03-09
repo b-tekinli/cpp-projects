ab74e3b91085bd4e9bc169298507cc679e61dbd7
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-03-09 15:04:45 +0300

    added

---
+++ friend.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Insan {
+    private:
+        string ad;
+        int yas;
+        friend void yasSoyle(Insan obj);    // friend ile private degiskene dısardan erisebiliriz.
+
+    public:
+        Insan(string name, int age) {
+            ad = name;
+            yas = age;
+        }
+        void adSoyle();
+};
+
+void Insan::adSoyle() {
+    cout << "Ad: " << ad << endl;
+}
+
+void yasSoyle(Insan obj) {
+    cout << "Yas: " << obj.yas << endl;
+}
+
+int main() {
+
+    Insan insan("Ahmet", 23);
+
+    insan.adSoyle();
+    yasSoyle(insan);
+
+
+    return (0);
+}
