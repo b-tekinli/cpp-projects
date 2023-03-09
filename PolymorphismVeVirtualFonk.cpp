72f31d7984b53dcc2b47c47752fc8b2e9dca8f2c
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-03-09 22:47:49 +0300

    updated

--- PolymorphismVeVirtualFonk.cpp
+++ PolymorphismVeVirtualFonk.cpp
@@ -3,18 +3,42 @@ using namespace std;
 
 class Balik {
     public:
+        Balik() {
+            cout << "Balik constructor" << endl;
+        }
+
+        virtual ~Balik() {
+            cout << "Balik destructor" << endl;
+        }
+
         virtual void yuz() { cout << "Balik yuzuyor.." << endl; }
 };
 
 
 class Yunus : public Balik {
     public:
-        void yuz() { cout << "Yunus yuzuyor.." << endl; }
+        Yunus() {
+            cout << "Yunus constructor" << endl;
+        }
+        
+        ~Yunus() {
+            cout << "Yunus destructor" << endl;
+        }
+
+        virtual void yuz() { cout << "Yunus yuzuyor.." << endl; }
 };
 
 
 class Balina : public Balik {
     public:
+        Balina() {
+            cout << "Balina constructor" << endl;
+        }
+        
+        ~Balina() {
+            cout << "Balina destructor" << endl;
+        }
+
         void yuz() { cout << "Balina yuzuyor.." << endl; }
 };
 
@@ -22,12 +46,22 @@ void baligiYuzdur(Balik &balik) {
     balik.yuz();
 }
 
-int main() {
-    Yunus yunus;
-    baligiYuzdur(yunus);
+void baligiSil(Balik *balik) {
+    delete balik;
+}
 
-    Balina balina;
-    baligiYuzdur(balina);
+int main() {
+    Yunus *yunus = new Yunus;   // dinamik olduğu için heapte fazla yer tutar.
+    // baligiYuzdur(yunus);
+    baligiSil(yunus);
+
+    // Balina balina;
+    // baligiYuzdur(balina);
+
+    Balina *balina = new Balina;
+    baligiSil(balina);
+
+    // delete yunus;
 
     return (0);
 }
